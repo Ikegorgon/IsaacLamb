4ad6a64a0b6e15f1f6b73fa5c1c719e2fba9f97f
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-11-30 11:21:13 -0700

    Made InternetController

--- IsaacLamb/InternetController.swift
+++ IsaacLamb/InternetController.swift
@@ -8,12 +8,41 @@
 
 import UIKit
 
-class InternetController: UIViewController {
+class InternetController: UITableViewController {
 
+    private (set) lazy var internetTopics: [String] = {
+        return [
+            "Definitions",
+            "CSP",
+            "CTEC",
+            "Canyons",
+            "Twitter",
+            "Swift Guide"
+        ]
+    }()
+    private var detailViewController : InternetDetailViewController?
+    private func setup() -> Void {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setup()
+        self.clearsSelectionOnViewWillAppear = false
         // Do any additional setup after loading the view.
+    }
+    
+    override public func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return internetTopics.count
+    }
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let currentText = internetTopics[indexPath.row]
+        cell.textLabel!.text = currentText
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
