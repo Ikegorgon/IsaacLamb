164c5976daac7eeedd5b460e21317ee9c8b2ae7e
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-11-30 11:20:15 -0700

    Added internet detail view controller

---
+++ IsaacLamb/InternetDetailViewontroller.swift
@@ -0,0 +1,21 @@
+//
+//  InternetDetailViewontroller.swift
+//  IsaacLamb
+//
+//  Created by Lamb, Isaac on 11/29/17.
+//  Copyright © 2017 Lamb, Isaac. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+public class InternetDetailViewController: UIViewController {
+    
+    
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    private func setup() -> Void
+}
