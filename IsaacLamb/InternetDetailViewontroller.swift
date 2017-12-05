c407c2c46b021a3fa4cd3222b298c3b143880d20
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-12-05 13:54:35 -0700

    Fixed InternetDetailViewController

--- IsaacLamb/InternetDetailViewontroller.swift
+++ IsaacLamb/InternetDetailViewontroller.swift
@@ -12,10 +12,44 @@ import WebKit
 public class InternetDetailViewController: UIViewController {
     
     
+    @IBOutlet weak var textView: UILabel!
+    @IBOutlet weak var webViewer: WKWebView!
     
+    var detailAddress : String? {
+        didSet {
+            configureDetailView()
+        }
+    }
+    var detailText : String? {
+        didSet {
+            configureDetailView()
+        }
+    }
     override public func viewDidLoad() {
         super.viewDidLoad()
-        setup()
+        configureDetailView()
     }
-    private func setup() -> Void
+    private func configureDetailView() -> Void {
+        if detailAddress != nil {
+            if let currentWebView = webViewer {
+                let currentURL = URL(string: detailAddress!)
+                let currentWebRequest = URLRequest(url: currentURL!)
+                currentWebView.load(currentWebRequest)
+            }
+        } else {
+            if let currentWebView = webViewer {
+                let currentURL = URL(string: "https://www.google.com")
+                currentWebView.load(URLRequest(url:currentURL!))
+            }
+        }
+        if detailText != nil {
+            if let currentText = textView {
+                currentText.text = detailText
+            }
+        } else {
+            if let currentText = textView {
+                currentText.text = "Isaac's CSP app internet screen"
+            }
+        }
+    }
 }
