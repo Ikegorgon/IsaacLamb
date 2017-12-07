195c1135b5b446f6d53471c99501e07960fd74e3
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-12-07 12:54:37 -0700

    Added to Creativity Controller

--- IsaacLamb/CreativityController.swift
+++ IsaacLamb/CreativityController.swift
@@ -8,7 +8,43 @@
 
 import UIKit
 
-class CreativityController: UIViewController {
+class CreativityController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    private let reuseIdentifier = "artIdentifier"
+    private let sectionInsests = UIEdgeInsets(top:50.0, left: 20.0, bottom:50.0, right: 20.0)
+    private let itemsPerRow : CGFloat = 3
+    
+    private lazy var artSelection : [UIImage?] = {
+        return [
+            UIImage(named: "JavaHiKu"),
+            UIImage(named: "Logo"),
+            UIImage(named: "globalcooling"),
+            UIImage(named: "Gate"),
+            UIImage(named: "Abstract"),
+            UIImage(named: "Sheep")
+        ]
+    }()
+    
+    var largePhotoIndexPath: IndexPath? {
+        didSet {
+            var indexPaths = [IndexPath]()
+            if let largePhotoIndexPath = largePhotoIndexPath {
+                indexPaths.append(largePhotoIndexPath)
+            }
+            if let oldValue = oldValue {
+                indexPaths.append(oldValue)
+            }
+            collectionView?.performBatchUpdates({
+                self.collectionView?.reloadItems(at:indexPaths)
+            })
+            {
+                completed in
+                
+                if let largePhotoIndexPath = self.largePhotoIndexPath {
+                    self.collectionView?.scrollToItem(at: largePhotoIndexPath, at: .centeredVertically, animated: true)
+                }
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +52,48 @@ class CreativityController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override public func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    override public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return artSelection.count
+    }
+    override public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let artCell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ArtCell
+        artCell.backgroundColor = .purple
+        artCell.imageView.image = artSelection[indexPath.row]
+        artCell.imageName.text = "My  Art"
+        
+        return artCell
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath == largePhotoIndexPath {
+            let art = artSelection[indexPath.row]
+            let size = collectionView.bounds.size
+            let widthScale = (size.width / art!.size.width) * CGFloat(0.80)
+            let largeSize = CGSize(width:art!.size.width * widthScale, height: art!.size.height * widthScale)
+            return largeSize
+        }
+        let paddingSpace = sectionInsests.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        return CGSize(width: widthPerItem, height : widthPerItem)
+    }
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsests
+    }
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsests.left
+    }
+    override public func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        if largePhotoIndexPath == indexPath {
+            largePhotoIndexPath = nil
+        } else {
+            largePhotoIndexPath = indexPath
+        }
+        return false
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
