2544df7acf4c35f6e3acd6e87bcf5fb2a3a13993
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-12-07 12:54:07 -0700

    Added ArtCell

---
+++ IsaacLamb/ArtCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArtCell.swift
+//  IsaacLamb
+//
+//  Created by Lamb, Isaac on 12/7/17.
+//  Copyright © 2017 Lamb, Isaac. All rights reserved.
+//
+
+import UIKit
+
+public class ArtCell : UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageName: UILabel!
+    
+}
