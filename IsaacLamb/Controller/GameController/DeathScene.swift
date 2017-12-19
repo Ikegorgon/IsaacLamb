0fa736c33de4bbd2ae480b01cf12bc78a9c3ca13
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-12-19 12:59:45 -0700

    Added DeathScene

---
+++ IsaacLamb/Controller/GameController/DeathScene.swift
@@ -0,0 +1,33 @@
+//
+//  DeathScene.swift
+//  IsaacLamb
+//
+//  Created by Lamb, Isaac on 12/19/17.
+//  Copyright © 2017 Lamb, Isaac. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class DeathScene: SKScene {
+    override public func didMove(to view:SKView) -> Void {
+        self.backgroundColor = SKColor.blue
+        let gameOver = SKLabelNode(text: "You Died")
+        gameOver.name = "game over"
+        gameOver.fontSize = 65
+        gameOver.fontColor = SKColor.white
+        gameOver.position = CGPoint(x: frame.midX, y: frame.midY)
+     addChild(gameOver)
+    }
+    override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) -> Void {
+        let touch = touches.first
+        let touchLocation = touch?.location(in: self)
+        let touchedNode = self.atPoint(touchLocation!)
+        if (touchedNode.name == "game over") {
+            let newGameScene = GameScene(size: size)
+            newGameScene.scaleMode = scaleMode
+            let transitionType = SKTransition.flipHorizontal(withDuration: 0.5)
+            view?.presentScene(newGameScene, transition: transitionType)
+        }
+    }
+}
